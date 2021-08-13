f3c74f4d27864fcfb1640c4600d2be324dceca65
Marina <briefmarina@gmail.com>
2021-08-13 12:39:29 +0200

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,38 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        //Вывод в консоль строки в результате конкатенации
+        String string = "";
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i <= 20000; i++) {
+            string += i + " ";
+        }
+
+        System.out.println(string);
+
+        long stopTime = System.currentTimeMillis();
+
+        System.out.println("Длительность работы, в мс: " + (stopTime - startTime));
+
+        //Вывод в консоль строки с помощью StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        long startTime1 = System.currentTimeMillis();
+
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i + " ");
+        }
+
+        System.out.println(sb);
+
+        long stopTime1 = System.currentTimeMillis();
+
+        System.out.println("Длительность работы, в мс: " + (stopTime1 - startTime1));
+
+        System.out.println("Конкатенация строк прошла за " + (stopTime - startTime) + " мс, а StringBuilder отработал за " + (stopTime1 - startTime1) + " мс.");
+
+
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,12 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Boeing", 2000, 150, 10000);
+
+        System.out.print("Старый способ: ");
+        airplane.info();
+
+        System.out.print("Новый способ: " + airplane);
 
     }
 }
