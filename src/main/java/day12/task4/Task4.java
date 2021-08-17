8a931f16111c13295abaae98253126bddec7a118
Marina <briefmarina@gmail.com>
2021-08-17 18:31:11 +0200

    Revert "complete day12 tasks"
    This reverts commit 09d52aaf3a0c2825cbb8bc5f985a2b1f0f4c593b.

--- src/main/java/day12/task4/Task4.java
+++ src/main/java/day12/task4/Task4.java
@@ -1,37 +1,7 @@
 package day12.task4;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Task4 {
     public static void main(String[] args) {
-
-        List<String> band1 = new ArrayList<>();
-        band1.add("Rudolf Schenker");
-        band1.add("Karl-Heinz Vollmer");
-        band1.add("Joachim Kirchhoff");
-        band1.add("Wolfgang Dziony");
-        MusicBand m1 = new MusicBand("Scorpions", 1972, band1);
-
-        List<String> band2 = new ArrayList<>();
-        band2.add("Freddie Mercury");
-        band2.add("Brian May");
-        band2.add("Roger Taylor");
-        band2.add("Mike Grose");
-        MusicBand m2 = new MusicBand("Queen", 1970, band2);
-
-        System.out.print("Музыканты " + m1 + ": \n");
-        m1.printMembers();
-        System.out.println();
-
-        MusicBand.transferMembers(m1, m2);
-
-        System.out.print("Музыканты " + m1 + " после слияния: \n");
-        m1.printMembers();
-        System.out.println();
-
-        System.out.print("Музыканты " + m2 + " после слияния: \n");
-        m2.printMembers();
 
     }
 }
