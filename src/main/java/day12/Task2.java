baa860e06fb33ce48aa537b738498c5d7852ee3f
Marina <briefmarina@gmail.com>
2021-08-17 18:31:11 +0200

    Revert "complete day12 tasks"
    This reverts commit 93f03fc3df97da4c5f7d56223de5c86017e2c6f0.

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,19 +1,7 @@
 package day12;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Task2 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i <= 30; i += 2) {
-            list.add(i);
-        }
-        for (int i = 300; i <= 350; i += 2) {
-            list.add(i);
-        }
-
-        System.out.println(list);
     }
 }
