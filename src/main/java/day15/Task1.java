5568337f93fa2eed8a786b7081f93acc65644fe5
Marina <briefmarina@gmail.com>
2021-08-20 20:25:41 +0200

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,30 @@
 package day15;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        try {
+            Scanner sc = new Scanner(new File("shoes"));
+            PrintWriter pw = new PrintWriter("missing_shoes");
+
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] shoe = line.split(";");
+
+               if (Integer.parseInt(shoe[2]) == 0) {
+                   pw.println(line.replace(";", ", "));
+               }
+            }
+            sc.close();
+            pw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
